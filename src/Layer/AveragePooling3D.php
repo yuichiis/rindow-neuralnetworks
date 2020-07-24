8841ec5d1524902c2f1443a283d66bfd19327db4
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2020-07-25 06:49:05 +0900

    update AveragePooling3D.php

--- src/Layer/AveragePooling3D.php
+++ src/Layer/AveragePooling3D.php
@@ -3,7 +3,7 @@ namespace Rindow\NeuralNetworks\Layer;
 
 use Interop\Polite\Math\Matrix\NDArray;
 
-class MaxPooling3D extends AbstractPooling implements Layer
+class AveragePooling3D extends AbstractPooling implements Layer
 {
     protected $rank = 3;
     protected $pool_mode = 'avg';
