e5a1c1d9700de5a298067b55d77a9173b5a601aa
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2020-07-24 10:19:50 +0900

    update SequentialTest.php

--- tests/RindowTest/NeuralNetworks/Model/SequentialTest.php
+++ tests/RindowTest/NeuralNetworks/Model/SequentialTest.php
@@ -495,6 +495,61 @@ class Test extends TestCase
         }
     }
 
+    public function testFitConv1DandMaxPool1D()
+    {
+        $mo = new MatrixOperator();
+        $backend = new Backend($mo);
+        $nn = new NeuralNetworks($mo,$backend);
+        $plt = new Plot($this->getPlotConfig(),$mo);
+
+        $model = $nn->models()->Sequential([
+            $nn->layers()->Conv1D(
+                $filters=5,
+                $kernel_size=3,
+                ['input_shape'=>[5,1]]),
+            $nn->layers()->ReLU(),
+            $nn->layers()->MaxPool1D(),
+            $nn->layers()->Flatten(),
+            $nn->layers()->Dense($units=10),
+            $nn->layers()->ReLU(),
+            $nn->layers()->Dense($units=5),
+            $nn->layers()->Softmax(),
+        ]);
+
+        $model->compile([
+            'loss'=>$nn->losses()->SparseCategoricalCrossEntropy(),
+        ]);
+        $this->assertTrue( $model->layers()[7]->fromLogits());
+        $this->assertTrue( $model->lossFunction()->fromLogits());
+
+        // training greater or less
+        $x = $mo->arange(
+            5*5,
+            null,null,
+            NDArray::float32
+            )->reshape([5,5,1]);
+        $t = $mo->arange(5);
+        $v_x = $mo->arange(
+            5*5,
+            null,null,
+            NDArray::float32
+            )->reshape([5,5,1]);
+        $v_t = $mo->arange(5);
+        $history = $model->fit($x,$t,['epochs'=>100,'validation_data'=>[$v100$v_t],'verbose'=>0]);
+
+        $this->assertEquals(['loss','accuracy','val_loss','val_accuracy'],array_keys($history));
+
+        if($this->plot) {
+            $plt->plot($mo->array($history['loss']),null,null,'loss');
+            $plt->plot($mo->array($history['val_loss']),null,null,'val_loss');
+            $plt->plot($mo->array($history['accuracy']),null,null,'accuracy');
+            $plt->plot($mo->array($history['val_accuracy']),null,null,'val_accuracy');
+            $plt->legend();
+            $plt->title('Convolution');
+            $plt->show();
+        }
+    }
+
     public function testToJson()
     {
         $mo = new MatrixOperator();
