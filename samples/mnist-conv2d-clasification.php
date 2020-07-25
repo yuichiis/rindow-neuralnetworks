63006a29aebe1531d1e37f7bb52b99da2dabc140
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2020-07-25 11:07:56 +0900

    update mnist-conv2d-clasification.php

--- samples/mnist-conv2d-clasification.php
+++ samples/mnist-conv2d-clasification.php
@@ -19,7 +19,7 @@ fwrite(STDERR,"test=[".implode(',',$test_img->shape())."]\n");
 
 if(true || !extension_loaded('rindow_openblas')) {
     // Shrink data
-    $epochs = 3;
+    $epochs = 5;
     $trainSize = 6000;
     $testSize  = 100;
     fwrite(STDERR,"Shrink data ...\n");
