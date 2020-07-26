18850b263e03e467e07e34c6f907a28a4919fc43
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2020-07-27 07:42:47 +0900

    update Datasets.php

--- src/Builder/Datasets.php
+++ src/Builder/Datasets.php
@@ -2,11 +2,13 @@
 namespace Rindow\NeuralNetworks\Builder;
 
 use Rindow\NeuralNetworks\Dataset\Mnist;
+use Rindow\NeuralNetworks\Dataset\FashionMnist;
 
 class Datasets
 {
     protected $matrixOperator;
     protected $mnist;
+    protected $fashionMnist;
 
     public function __construct($matrixOperator)
     {
@@ -17,6 +19,16 @@ class Datasets
     {
         if($this->mnist==null) {
             $this->mnist = new Mnist(
+                $this->matrixOperator);
+
+        }
+        return $this->mnist;
+    }
+
+    public function fashionMnist()
+    {
+        if($this->fashionMnist==null) {
+            $this->fashionMnist = new FashionMnist(
                 $this->matrixOperator);
 
         }
