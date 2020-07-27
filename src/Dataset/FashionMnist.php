695f9530011212d86d56d970a439c7f6161d0d72
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2020-07-27 11:39:47 +0900

    update FashionMnist.php

--- src/Dataset/FashionMnist.php
+++ src/Dataset/FashionMnist.php
@@ -4,4 +4,9 @@ namespace Rindow\NeuralNetworks\Dataset;
 class FashionMnist extends Mnist
 {
     protected $urlBase = 'http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/';
+    
+    protected function getDatasetDir()
+    {
+        return sys_get_temp_dir().'/rindow/nn/datasets/fashion-mnist';
+    }
 }
