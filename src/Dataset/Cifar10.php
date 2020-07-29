c5e563fd269c136207cdf11bf09de41a8c4bf475
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2020-07-29 20:43:51 +0900

    update Cifar10.php

--- src/Dataset/Cifar10.php
+++ src/Dataset/Cifar10.php
@@ -215,6 +215,7 @@ class Cifar10
         $blue = unpack("C*",$bluedata);
         $size = count($red);
 echo "size=$size.";
+var_dump($red);
         for($i=0,$j=0;$i<$size;$i++) {
             $buffer[$j++] = $red[$i];
             $buffer[$j++] = $green[$i];
