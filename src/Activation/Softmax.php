4570ce9d4988c1d1bc7d2dc68be65711c7b7c9a4
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2020-08-05 19:33:59 +0900

    update Softmax.php

--- src/Activation/Softmax.php
+++ src/Activation/Softmax.php
@@ -1,19 +1,11 @@
 <?php
-namespace Rindow\NeuralNetworks\Layer;
+namespace Rindow\NeuralNetworks\Activation;
 
 use Interop\Polite\Math\Matrix\NDArray;
 
-class Softmax extends AbstractLayer implements Layer
+class Softmax extends AbstractActivation
 {
-    protected $backend;
     protected $outputs;
-
-    public function __construct($backend,array $options=null)
-    {
-        //extract($this->extractArgs([
-        //],$options));
-        $this->backend = $K = $backend;
-    }
 
     protected function call(NDArray $inputs, bool $training) : NDArray
     {
