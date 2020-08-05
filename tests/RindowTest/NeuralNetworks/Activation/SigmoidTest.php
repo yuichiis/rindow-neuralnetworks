925eda9aee261759e855fab65cd229dbea4fcbd1
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2020-08-05 19:38:03 +0900

    update SigmoidTest.php

--- tests/RindowTest/NeuralNetworks/Activation/SigmoidTest.php
+++ tests/RindowTest/NeuralNetworks/Activation/SigmoidTest.php
@@ -1,10 +1,10 @@
 <?php
-namespace RindowTest\NeuralNetworks\Layer\SigmoidTest;
+namespace RindowTest\NeuralNetworks\Activation\SigmoidTest;
 
 use PHPUnit\Framework\TestCase;
 use Rindow\Math\Matrix\MatrixOperator;
 use Rindow\NeuralNetworks\Backend\RindowBlas\Backend;
-use Rindow\NeuralNetworks\Layer\Sigmoid;
+use Rindow\NeuralNetworks\Activation\Sigmoid;
 
 
 class Test extends TestCase
@@ -13,11 +13,10 @@ class Test extends TestCase
     {
         $mo = new MatrixOperator();
         $backend = new Backend($mo);
-        $layer = new Sigmoid($backend);
-        $layer->build([]);
+        $activation = new Sigmoid($backend);
 
         $x = $mo->array([-1.0,-0.5,0.0,0.5,1.0]);
-        $y = $layer->forward($x, $training=true);
+        $y = $activation->forward($x, $training=true);
 
         $this->assertEquals([-1.0,-0.5,0.0,0.5,1.0],$x->toArray());
         $this->assertTrue($y[0]<0.5);
@@ -27,7 +26,7 @@ class Test extends TestCase
         $this->assertTrue($y[4]>0.5);
 
         $dout = $x;
-        $dx = $layer->backward($dout);
+        $dx = $activation->backward($dout);
         $this->assertTrue(abs(-0.196-$dx[0])<0.01);
         $this->assertTrue(abs(-0.117-$dx[1])<0.01);
         $this->assertTrue($dx[2]==0.0);
