40bafb401f233a9d81082ce92f7b23c077254c76
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2020-08-05 19:34:49 +0900

    update Tanh.php

--- src/Activation/Tanh.php
+++ src/Activation/Tanh.php
@@ -1,1 +1,25 @@
-#
+<?php
+namespace Rindow\NeuralNetworks\Activation;
+
+use Interop\Polite\Math\Matrix\NDArray;
+
+class Tanh extends AbstractActivation
+{
+    protected $mask;
+
+    protected function call(NDArray $inputs, bool $training) : NDArray
+    {
+        $K = $this->backend;
+        $outputs = $K->tanh($inputs);
+        $this->outputs = $outputs;
+        return $outputs;
+    }
+
+    protected function differentiate(NDArray $dOutputs) : NDArray
+    {
+        $K = $this->backend;
+        // dx = dy * (1 - y^2)
+        $dInputs = $K->mul($dOutputs,$K->increment($K->scale(-1,$K->square($this->outputs)),1));
+        return $dInputs;
+    }
+}
