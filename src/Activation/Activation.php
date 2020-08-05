fbc27499fe1b0ebdc09db726651613855441044c
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2020-08-05 19:29:13 +0900

    update Activation.php

--- src/Activation/Activation.php
+++ src/Activation/Activation.php
@@ -1,1 +1,10 @@
-#
+<?php
+namespace Rindow\NeuralNetworks\Activation;
+
+use Interop\Polite\Math\Matrix\NDArray;
+
+interface Activation
+{
+    protected function call(NDArray $inputs, bool $training) : NDArray;
+    protected function differentiate(NDArray $dOutputs) : NDArray;
+}
