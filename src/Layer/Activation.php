8a28ba6b0da02dfb445c7d694aa5d070610aea38
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2020-08-06 10:02:54 +0900

    update Activation.php

--- src/Layer/Activation.php
+++ src/Layer/Activation.php
@@ -1,1 +1,32 @@
-#
+<?php
+namespace Rindow\NeuralNetworks\Layer;
+
+use InvalidArgumentException;
+use Interop\Polite\Math\Matrix\NDArray;
+use Rindow\NeuralNetworks\Support\GenericUtils;
+
+class Activation extends AbstractLayer implements Layer
+{
+    use GenericUtils;
+    protected $backend;
+
+    public function __construct($backend, $activation,array $options=null)
+    {
+        extract($this->extractArgs([
+            'input_shape'=>null,
+        ],$options));
+        $this->backend = $K = $backend;
+        $this->inputShape = $input_shape;
+        $this->setActivation($activation);
+    }
+
+    protected function call(NDArray $inputs, bool $training) : NDArray
+    {
+        return $inputs;
+    }
+
+    protected function differentiate(NDArray $dOutputs) : NDArray
+    {
+        return $dOutputs;
+    }
+}
