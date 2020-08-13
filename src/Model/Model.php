467a05a56538ff55c68049ed7d833d40a54d3278
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2020-08-13 18:27:20 +0900

    update Model.php

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -1,1 +1,13 @@
-#
+<?php
+namespace Rindow\NeuralNetworks\Model;
+interface Model
+{
+    public function compile(array $options=null) : void;
+    public function fit(NDArray $inputs, NDArray $tests, array $options=null) : array;
+    public function evaluate(NDArray $x, NDArray $t, array $options=null) : array;
+    public function predict($inputs, array $options=null) : NDArray;
+    public function toJson() : string;
+    public function saveWeights(&$modelWeights,$portable=null) : void;
+    public function loadWeights($modelWeights) : void;
+    public function save($filepath,$portable=null) : void;
+}
