f168ddc535931fab16ccc5195fdb417d7ff80772
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2020-08-15 07:08:48 +0900

    update AbstractRNNLayer.php

--- src/Layer/AbstractRNNLayer.php
+++ src/Layer/AbstractRNNLayer.php
@@ -12,13 +12,20 @@ abstract class AbstractRNNLayer extends AbstractLayerBase implements RNNLayer
     abstract protected function call(NDArray $inputs, bool $training, array $initialStates=null, array $options=null);
     abstract protected function differentiate(NDArray $dOutputs, array $dStates=null);
 
+    public function setShapeInspection(bool $enable)
+    {
+        parent::setShapeInspection($enable);
+        $this->cell->setShapeInspection($enable);
+    }
+
     final public function forward(NDArray $inputs, bool $training, array $initialStates=null,array $options=null)
     {
         $this->assertInputShape($inputs);
-
+        $this->assertStatesShape($initialStates);
         $results = $this->call($inputs,$training,$initialStates,$options);
         if(is_array($results)) {
             [$outputs,$states] = $results;
+            $this->assertStatesShape($states);
         } elseif($results instanceof NDArray) {
             $outputs = $results;
         }
@@ -29,11 +36,13 @@ abstract class AbstractRNNLayer extends AbstractLayerBase implements RNNLayer
     final public function backward(NDArray $dOutputs, array $dStates=null)
     {
         $this->assertOutputShape($dOutputs);
+        $this->assertStatesShape($dStates);
 
         $results = $this->differentiate($dOutputs,$dStates);
 
         if(is_array($results)) {
             [$dInputs,$dStates] = $results;
+            $this->assertStatesShape($dStates);
         } elseif($results instanceof NDArray) {
             $dInputs = $results;
         }
