65d0ed9d88d66b36fab632b7d35fd3f308f3c0fb
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2020-08-14 11:08:38 +0900

    update RNNCell.php

--- src/Layer/RNNCell.php
+++ src/Layer/RNNCell.php
@@ -1,1 +1,13 @@
-#
+<?php
+namespace Rindow\NeuralNetworks\Layer;
+
+use Interop\Polite\Math\Matrix\NDArray;
+
+/**
+ *
+ */
+interface RNNCell extends LayerBase
+{
+    public function forward(NDArray $inputs, array $states, bool $training,array $options=null) : array;
+    public function backward(NDArray $dOutputs,array $dStates) : NDArray;
+}
