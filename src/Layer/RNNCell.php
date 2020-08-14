1c9be4930b137d0f4b11ca63478c8e31934e7998
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2020-08-14 11:18:37 +0900

    update RNNCell.php

--- src/Layer/RNNCell.php
+++ src/Layer/RNNCell.php
@@ -8,6 +8,6 @@ use Interop\Polite\Math\Matrix\NDArray;
  */
 interface RNNCell extends LayerBase
 {
-    public function forward(NDArray $inputs, array $states, bool $training,array $options=null) : array;
-    public function backward(NDArray $dOutputs,array $dStates) : NDArray;
+    public function forward(NDArray $inputs, array $states, bool $training, object $calcState, array $options=null) : array;
+    public function backward(NDArray $dOutputs, array $dStates, object $calcState) : array;
 }
