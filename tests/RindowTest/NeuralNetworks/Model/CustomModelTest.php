e19a8f2dcfc3ff697d2a5f2fd9d70758e4e0b1f5
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2020-08-15 11:47:50 +0900

    update CustomModelTest.php

--- tests/RindowTest/NeuralNetworks/Model/CustomModelTest.php
+++ tests/RindowTest/NeuralNetworks/Model/CustomModelTest.php
@@ -6,6 +6,7 @@ use Rindow\Math\Matrix\MatrixOperator;
 use Rindow\NeuralNetworks\Backend\RindowBlas\Backend;
 use Rindow\NeuralNetworks\Builder\NeuralNetworks;
 use Rindow\NeuralNetworks\Model\ModelLoader;
+use Rindow\NeuralNetworks\Model\AbstractModel;
 use Interop\Polite\Math\Matrix\NDArray;
 
 class TestModel extends AbstractModel
