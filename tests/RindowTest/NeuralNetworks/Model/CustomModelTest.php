69e3f7ab123fc8fa66a22bbd619e637423f76bf3
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2020-08-15 13:13:32 +0900

    update CustomModelTest.php

--- tests/RindowTest/NeuralNetworks/Model/CustomModelTest.php
+++ tests/RindowTest/NeuralNetworks/Model/CustomModelTest.php
@@ -79,7 +79,7 @@ class TestLayer extends AbstractLayer
     
     protected function differentiate(NDArray $dOutputs) : NDArray
     {
-        $din = $this->fc->backward($dOutputs,$training);
+        $din = $this->fc->backward($dOutputs);
         return $din;
     }
 }
