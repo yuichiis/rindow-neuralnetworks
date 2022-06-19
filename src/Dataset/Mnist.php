bd816f7382a4c3087cb16374df05e732f83ed4aa
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2022-06-19 22:39:02 +0900

    Update Mnist.php

--- src/Dataset/Mnist.php
+++ src/Dataset/Mnist.php
@@ -189,7 +189,7 @@ class Mnist
         }
         gzclose($zp);
         if($i>$bfsz) {
-            throw new RuntimeException("File ".$filePath." is broken maybe. Please remove and reload.");
+            throw new RuntimeException("File ".$filePath." is probably broken. Please remove and reload.");
         }
     }
 }
