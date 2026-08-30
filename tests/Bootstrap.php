e015a212cc11ea5117519aa67f52cc84c70a3ba0
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2026-08-30 19:17:29 +0900

    develop

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -4,9 +4,9 @@ ini_set('short_open_tag', '1');
 date_default_timezone_set('UTC');
 #ini_set('short_open_tag',true);
 if(file_exists(__DIR__.'/../vendor/autoload.php')) {
-    $loader = require_once __DIR__.'/../vendor/autoload.php';
+    $loader = require __DIR__.'/../vendor/autoload.php';
 } else {
-    $loader = require_once __DIR__.'/init_autoloader.php';
+    $loader = require __DIR__.'/init_autoloader.php';
 }
 
 define('RINDOWTEST_TEMP_DIR',__DIR__.'/tmp');
