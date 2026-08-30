45494acae8fe840d6f35ee70e3f422aea1d3a8eb
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2026-08-30 17:37:06 +0900

    develop

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -16,6 +16,7 @@ if(!file_exists(RINDOWTEST_TEMP_DIR)) {
 $addpack = getenv('ADD_PACK');
 $workingbranch = getenv('WORKING_BRANCH');
 if(file_exists("$addpack/rindow-math-matrix-matlibffi-$workingbranch/composer.json")) {
+    $loader->addPsr4('Interop\\Polite\\Math\\', "$addpack/polite-math-$workingbranch/src");
     $loader->addPsr4('Rindow\\Math\\Matrix\\',   "$addpack/rindow-math-matrix-$workingbranch/src");
     $loader->addPsr4('Rindow\\Math\\Matrix\\Drivers\\MatlibFFI\\', "$addpack/rindow-math-matrix-matlibffi-$workingbranch/src");
     $loader->addPsr4('Rindow\\Math\\Buffer\\FFI\\', "$addpack/rindow-math-buffer-ffi-$workingbranch/src");
@@ -23,6 +24,7 @@ if(file_exists("$addpack/rindow-math-matrix-matlibffi-$workingbranch/composer.js
     $loader->addPsr4('Rindow\\OpenBLAS\\FFI\\', "$addpack/rindow-openblas-ffi-$workingbranch/src");
     $loader->addPsr4('Rindow\\OpenCL\\FFI\\',   "$addpack/rindow-opencl-ffi-$workingbranch/src");
     $loader->addPsr4('Rindow\\CLBlast\\FFI\\',  "$addpack/rindow-clblast-ffi/src");
+    $loader->addPsr4('Rindow\\Math\\Plot\\', "$addpack/rindow-math-plot-$workingbranch/src");
 }
 #if(!class_exists('PHPUnit\Framework\TestCase')) {
 #    include __DIR__.'/travis/patch55.php';
