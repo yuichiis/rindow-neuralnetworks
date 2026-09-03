fdea28df288232cfd870533dae29443f8364755e
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2026-09-04 06:35:54 +0900

    test.php

--- tests/RindowTest/NeuralNetworks/Layer/DenseTest.php
+++ tests/RindowTest/NeuralNetworks/Layer/DenseTest.php
@@ -38,7 +38,16 @@ class DenseTest extends TestCase
         $dOutputs = $K->ones($outputsVariable->shape(),$outputsVariable->dtype());
         $dInputs = $outputsVariable->creator()->backward([$dOutputs]);
 
-        return $mo->la()->isclose($grads[0],$K->ndarray($dInputs[0]),1e-3);
+        $isclose = $mo->la()->isclose($grads[0],$K->ndarray($dInputs[0]),1e-3);
+        if(!$isclose) {
+            echo "verify gradient fail in " . __CLASS__ . "::" . __FUNCTION__ . ":\n";
+            echo "grads[0]:\n";
+            echo $mo->toString($grads[0]);
+            echo "dInputs[0]:\n";
+            echo $mo->toString($K->ndarray($dInputs[0]));
+            echo "\n";
+        }
+        return $isclose;
     }
 
     public function testDefaultInitialize()
